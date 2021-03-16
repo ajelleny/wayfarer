bde42bc07e1ce09fa4af6ff31997e06e04b07a6d
ajelleny <ajelleny@gmail.com>
2021-03-16 16:55:48 -0700

    adds models

--- main_app/models.py
+++ main_app/models.py
@@ -1,24 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    review = models.TextField(max_length=250)
-    date = models.DateField("Post Date")
-    # adds the many to many association 
-    locations = models.ManyToManyField(Location)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.name}"
+# LOCATION = (
+#     ('N', 'New York'),
+#     ('L', 'London'),
+#     ('S', 'San Francisco')
+# )
 
 # Create your models here.
 class Location(models.Model):
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     # adds the many to many association 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, null=False, through="Post")
     
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    review = models.TextField(max_length=250)
+    date = models.DateField("Post Date")
+    # adds the many to many association 
+    # locations = models.ManyToManyField(Location)
+    location = models.ForeignKey(Location, default=1, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.title}"
+
+
+
+
+
+# Create your models here.
+
+
