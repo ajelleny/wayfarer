9b5282f6e47126d2a76810f58f3e1e26b7046616
ajelleny <ajelleny@gmail.com>
2021-03-17 11:34:44 -0700

    added username form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
-# from django import forms
-# from .models import Post 
+from django import forms
+from .models import User 
 
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ['title', 'review']
+class UsernameForm(forms.ModelForm):
+  class Meta:
+    model = User
+    fields = ['username']
