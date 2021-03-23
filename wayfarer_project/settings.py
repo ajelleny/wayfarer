589b73a1dc4fc13b0e0698d77f02e8cf2f8a092d
ajelleny <ajelleny@gmail.com>
2021-03-23 12:17:43 -0700

    hiding key

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fhmud9@cp&xf1&pjgv@2+zzto_m_oztqz+&v+@k$e5i5h)-u2i'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
